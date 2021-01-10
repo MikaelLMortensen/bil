f241677186fef23ffd72b73808a8e8d52f2c4f28
MikaelLMortensen <MikaelLMortensen@users.noreply.github.com>
2021-01-10 13:10:41 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,61 +1,83 @@
-/*
-    lf : Left Forward - Boolean
-    rf : Right Forward - Boolean
-    ls : Left Speed (0 - 1023)
-    rs : Right Speed (0 - 1023)
-*/
-let speedStruct = { lf : true, rf:true, ls:0, rs:0 }
+let leftSpeed = 0
+let rightSpeed = 0
+
 radio.setGroup(20)
+basic.showIcon(IconNames.Tortoise)
 
-radio.onReceivedString(function (receivedString: string) {
-    if (receivedString.indexOf("{") == 0 && receivedString.indexOf("}") == receivedString.length - 1) {
-        speedStruct = JSON.parse(receivedString)
-
-        let leftSpeed = Math.floor(speedStruct.ls / 100) 
-        if (leftSpeed>100){
-            leftSpeed = 100
-        } else if (leftSpeed < 5) {
-            leftSpeed = 0
-        }
-
-        let rightSpeed = Math.floor(speedStruct.rs / 100) 
-        if (rightSpeed>100){
-            rightSpeed = 100
-        } else if (rightSpeed < 5) {
-            rightSpeed = 0
-        }
-
-        if (speedStruct.lf) {
-            if (leftSpeed > 0) {
-                Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, leftSpeed)
-            } else {
-                Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
-            }
+radio.onReceivedValue(function (name: string, value: number) {
+  switch (name) {
+      case "ls": 
+        leftSpeed = value
+        if (value == 0) {
+            Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
+        } 
+        else if (value > 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, value)
         } else {
-            if (leftSpeed > 0) {
-                Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, leftSpeed)
-            } else {
-                Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
-            }
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, value*-1)
         }
-        if (speedStruct.rf) {
-            if (rightSpeed > 0) {
-                Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, rightSpeed)
-            } else {
-                Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
-            }
+      break;
+      case "rs":
+        rightSpeed = value
+        if (value == 0) {
+            Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
+        } 
+        else if (value > 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, value)
         } else {
-            if (rightSpeed > 0) {
-                Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, rightSpeed)
-            } else {
-                Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
-            }
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, value*-1)
         }
-    } else {
-        basic.showString(receivedString)
-    }
+      break;
+      default : {
+          basic.showString("E:" + name +  ": " + value.toString())
+      }
+      showSpeedInfo()
+  }
+
 })
 
+
+function showSpeedInfo(){
+
+    if (leftSpeed == 0 && rightSpeed == 0) {
+        basic.clearScreen()
+        return
+    }
+
+    if (leftSpeed > 0 && rightSpeed > 0) {
+        basic.plotLeds(`
+        . # . # .
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `) 
+    } else  if (leftSpeed < 0 && rightSpeed < 0) {
+        basic.plotLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        . # . # .
+        `) 
+    } else if (leftSpeed > 0 && rightSpeed < 0) {
+        basic.plotLeds(`
+        . # . . .
+        # # # . .
+        . . . . .
+        . . # # #
+        . . . # .
+        `) 
+    } else {
+        basic.plotLeds(`
+        . . . # .
+        . . # # #
+        . . . . .
+        # # # . .
+        . # . . .
+        `) 
+    }
+}
 
 
 basic.forever(function () {
