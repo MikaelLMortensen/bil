131889db82e81a50bcfba039d50b18b4e6f3bf8b
MikaelLMortensen <MikaelLMortensen@users.noreply.github.com>
2021-01-13 06:52:56 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,45 +6,6 @@ let direction = 50
 radio.setGroup(20)
 basic.showArrow(ArrowNames.North)
 
-function showSpeedInformation() {
-    if (speed < 5) {
-        basic.clearScreen()
-        return
-    }
-    if (speed > 0) {
-        if (direction < 15) {
-            basic.showArrow(ArrowNames.SouthWest)
-        } else if (direction >= 15 && direction < 30) {
-            basic.showArrow(ArrowNames.West)
-        } else if (direction >= 30 && direction < 45) {
-            basic.showArrow(ArrowNames.NorthWest)
-        } else if (direction >= 45 && direction < 55) {
-            basic.showArrow(ArrowNames.North)
-        } else if (direction >= 55 && direction < 70) {
-            basic.showArrow(ArrowNames.NorthEast)
-        } else if (direction >= 70 && direction < 85) {
-            basic.showArrow(ArrowNames.East)
-        } else if (direction >= 85) {
-            basic.showArrow(ArrowNames.SouthEast)
-        }
-    } else {
-        if (direction < 15) {
-            basic.showArrow(ArrowNames.NorthEast)
-        } else if (direction >= 15 && direction < 30) {
-            basic.showArrow(ArrowNames.East)
-        } else if (direction >= 30 && direction < 45) {
-            basic.showArrow(ArrowNames.SouthEast)
-        } else if (direction >= 45 && direction < 55) {
-            basic.showArrow(ArrowNames.South)
-        } else if (direction >= 55 && direction < 70) {
-            basic.showArrow(ArrowNames.SouthWest)
-        } else if (direction >= 70 && direction < 85) {
-            basic.showArrow(ArrowNames.West)
-        } else if (direction >= 85) {
-            basic.showArrow(ArrowNames.NorthWest)
-        }
-    }
-}
 
 function setSpeed() {
     let speedLeft = speed
@@ -52,15 +13,14 @@ function setSpeed() {
     if (speed == 0) {
         Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
         Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
-        //showSpeedInformation()        
         return
     } 
 
     if (speed > 0) {
         if (direction > 50) {
-            speedLeft = speed - (direction - 50)
+            speedRight = speed - (direction - 50)
         } else if (direction < 50) {
-            speedRight = speed - (50 - direction)
+            speedLeft = speed - (50 - direction)
         }
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, speedLeft)
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, speedRight)
@@ -68,14 +28,13 @@ function setSpeed() {
         speedLeft = speed * -1
         speedRight = speed * -1
         if (direction > 50) {
-            speedRight = speedRight - (direction - 50)
+            speedLeft = speedLeft - (direction - 50)
         } else if (direction < 50) {
-            speedLeft = speedLeft - direction
+            speedRight = speedRight - direction
         }
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, speedLeft)
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, speedRight)
     }
-    //showSpeedInformation()        
 }
 
 radio.onReceivedValue(function (name: string, value: number) {
